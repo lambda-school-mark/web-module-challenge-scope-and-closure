c15cbb1eab14a2b625ffddf186809cde0ec081db
mark-pekun <mark.pekun@gmail.com>
2020-05-18 20:02:21 -0400

    completed

--- index.js
+++ index.js
@@ -2,44 +2,45 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2?
+ * counter one has a function nested inside and acts as a closure
+ * counter two will just return 1
+ * 2. Which of the two uses a closure? How can you tell?
+ * the second one because it has a variable set as a function
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+ * counter1 is better if you want it to start with 0 each time
+ * counter2 is better for saving the progress
+ */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -47,20 +48,18 @@ const counter1 = counterMaker();
 // counter2 code
 let count = 0;
 
-function counter2() {
+function counter2(count) {
   return count++;
 }
-
 
 /* Task 2: inning() 
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning() {
+  return Math.floor(Math.random() * Math.floor(3));
 }
+console.log(inning());
 
 /* Task 3: finalScore()
 
@@ -74,13 +73,22 @@ finalScore(inning, 9) might return:
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
+// function finalScore(inning, number) {
 
-  /*Code Here*/
+//   let score = {
+//     Home:0,
+//     Away:0
+//   }
 
-}
+//   for (let i = 0; i < number; i++){
+//     score.Home += inning();
+//     score.Away += inning();
+//   }
+//   return score
+// }
+// console.log(finalScore(inning, 9))
 
 /* Task 4: 
 
@@ -103,8 +111,26 @@ and returns the score at each pont in the game, like so:
 
 Final Score: 6 - 10 */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function finalScore(inning, number) {
+  let score = {
+    Home: 0,
+    Away: 0,
+  };
+  let storeArray = [];
+
+  for (let i = 0; i < number; i++) {
+    let home = inning();
+    let away = inning();
+
+    score.Home += home;
+    score.Away += away;
+
+    // console.log(`${i} inning ${home} - ${away}`)
+    storeArray.push(`${i} inning ${home} - ${away}`);
+  }
+
+  // return score
+  storeArray.push(score);
+  return storeArray;
 }
-
-
+console.log(finalScore(inning, 9));
---
+++ stretch.js
@@ -0,0 +1,38 @@
+// ### Task 3 - Stretch Goals
+
+// After you have completed the requirements, **create** a new file called `stretch.js` and practice more with closures. There are no tests for these problems.
+
+// See if you can complete one or more of the following challenges:
+
+// 1. Predict the output of the code below and explain why this is the output using what you learned today. When you're ready for answers, view an explanation [here](https://www.coderbyte.com/algorithm/3-common-javascript-closure-questions
+
+// (function () {
+//   var a = (b = 3);
+// })();
+// console.log("a defined? " + (typeof a !== "undefined"));
+// console.log("b defined? " + (typeof b !== "undefined"));
+
+// 2. Write a function that would allow you to do this using a closure. (This is another interview question we've seen before - when you're ready for answers, view an explanation [here](https://www.coderbyte.com/algorithm/3-common-javascript-closure-questions)).
+
+// var addSix = createBase(6);
+// addSix(10); // returns 16
+// addSix(21); // returns 27
+
+function createBase(base) {
+  return function (number) {
+    return number + base;
+  };
+}
+
+const addSix = createBase(6);
+console.log(addSix(10));
+
+// 3. Research the differences between functional programming and object oriented programming. Then, describe the pros and cons of functional programming vs object-oriented programming. This is a common interview question and great practice!
+
+// ## Resources
+
+// 📚 [Scope and Closures Guide](https://css-tricks.com/javascript-scope-closures/)
+
+// 🧠 ["I never Understood Closures" Blog](https://medium.com/dailyjs/i-never-understood-javascript-closures-9663703368e8)
+
+// ## Submission Format
